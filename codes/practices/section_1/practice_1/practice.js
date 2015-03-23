1791805ca64250f83bd7c9d3d04c302c0037ba5d
chenyue <15532196053@163.com>
2015-03-23 16:52:20 +0800

    add css test

---
+++ codes/practices/section_1/practice_1/practice.js
@@ -0,0 +1,14 @@
+$(window).ready(function() {
+    //记得添加参数
+    var circleDiameter = getCircleDiameter();
+});
+
+$(window).resize(function() {
+    //记得添加参数
+    var circleDiameter = getCircleDiameter();
+});
+
+//返回值 为 圆的直径
+function getCircleDiameter(windowWidth, windowHeight) {
+
+}
