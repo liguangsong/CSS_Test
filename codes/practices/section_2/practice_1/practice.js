ca85bd199e751ad3d9b2bc437a375f73801b18a2
chenyue <15532196053@163.com>
2015-03-25 13:00:17 +0800

    添加有答案的练习

---
+++ codes/practices/section_2/practice_1/practice.js
@@ -0,0 +1,48 @@
+$(window).ready(function() {
+    //记得添加参数
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    var circleDiameter = getCircleDiameter(windowWidth, windowHeight);
+    setCirclePlace(circleDiameter, windowHeight, windowWidth);
+    setTextPlace(windowHeight);
+    setCircleTextPlace(circleDiameter);
+});
+
+$(window).resize(function() {
+    //记得添加参数
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    var circleDiameter = getCircleDiameter(windowWidth, windowHeight);
+    setCirclePlace(circleDiameter, windowHeight, windowWidth);
+    setTextPlace(windowHeight);
+    setCircleTextPlace(circleDiameter);
+});
+
+//返回值 为 圆的直径
+function getCircleDiameter(windowWidth, windowHeight) {
+    if (windowWidth * 0.4 > windowHeight * 0.6) return windowHeight * 0.6;
+    return windowWidth * 0.4;
+}
+
+function setCirclePlace(circleDiameter, windowHeight, windowWidth) {
+    $('.circle').css({
+        'width': circleDiameter,
+        'padding-bottom': circleDiameter,
+        'top': (windowHeight - circleDiameter)/2,
+        'left': (windowWidth - circleDiameter)/2
+    });
+}
+
+function setTextPlace(windowHeight) {
+    $('.time').css({
+        'height': windowHeight * 0.1
+    });
+}
+
+function setCircleTextPlace(circleDiameter) {
+    $('.circle_text').css({
+        'height': circleDiameter * 0.2,
+        'padding-top': circleDiameter * 0.4,
+        'padding-bottom': circleDiameter * 0.4
+    })
+}
---
+++ codes/specs/section_2/practice_1_spec.js
@@ -0,0 +1,47 @@
+describe('practice-1-1', function () {
+
+    it("居中", function () {
+        var windowWidth = $(window).width();
+        var windowHeight = $(window).height();
+        var text = $('.time');
+        var circle = $('.circle');
+        var circleText = $('.circle_text');
+        var textHeight = text.height();
+        var textWidth = text.width();
+        var textLeft = text.offset().left;
+        var circleDiameter = circle.width();
+        var circleLeft = circle.offset().left;
+        var circleTop = circle.offset().top;
+        var circleTextWidth = circleText.width();
+        var circleTextHeight = circleText.height();
+        var circleTextTop = sizeToNum(circleText.css('padding-top'));
+        var circleTextBottom = sizeToNum(circleText.css('padding-bottom'));
+        expect(checkSizeRightOrNo(textHeight, (windowHeight * 0.1))).toBe(true);
+        expect(checkSizeRightOrNo((textLeft * 2 + textWidth), windowWidth)).toBe(true);
+        expect(checkSizeRightOrNo(circleTextTop, circleTextBottom)).toBe(true);
+        expect(checkSizeRightOrNo((circleTextTop + circleTextBottom + circleTextHeight), circleDiameter)).toBe(true);
+        expect(checkSizeRightOrNo((circleLeft * 2 + circleDiameter), windowWidth)).toBe(true);
+        expect(checkSizeRightOrNo((circleTop * 2 + circleDiameter), windowHeight)).toBe(true);
+    });
+
+    function checkSizeRightOrNo(x, y) {
+        if ((x - y) <= 4) return true;
+        return false;
+    }
+
+    function sizeToNum(size) {
+        return Number(size.substring(0, size.length - 2));
+    }
+
+    function getInt(num) {
+        return Math.round(num)
+    }
+
+    it("当浏览器宽高为（1200， 600）", function () {
+        expect(getCircleDiameter(1200, 600)).toBe(360);
+    });
+
+    it("当浏览器宽高为（600， 800）", function () {
+        expect(getCircleDiameter(600, 800)).toBe(240);
+    });
+});
